df05310458e08fa1cdc95c40763e66966cc27d09
Vedant <vedantkanojiya21@gmail.com>
2025-03-25 00:27:40 +0530

    I love you kisu baby

--- MAIN PAGE/fotter.php
+++ MAIN PAGE/fotter.php
@@ -10,89 +10,92 @@
 <body>
 
   <footer class="footer">
-  	 <div class="container">
-  	 	<div class="row">
-  	 		<div class="footer-col">
-  	 			<h4>IT Services</h4>
-  	 			<ul>
-  	 				<li><a href="it-infrastucture.php">IT Infrastructure Solution</a></li>
-  	 				<li><a href="System IntegrationServices.php">System Intigration Service</a></li>
-  	 				<li><a href="data-center.php">Data Center & Recovery</a></li>
-  	 				<li><a href="data-availbility.php">Data Availiblity</a></li>
-					<li><a href="Virtualization.php">Virtulization</a></li>
-					<li><a href="CloudServices.php">Cloud computing</a></li>
-					<li><a href="Email Spam Protections.php">Email Protection</a></li>
-  	 			</ul>
-  	 		</div>
-  	 		<div class="footer-col">
-  	 			<h4>Coveraged Network</h4>
-  	 			<ul>
-  	 				<li><a href="Structured Cabling System.php">Structure Cabling System</a></li>
-  	 				<li><a href="Wireless Mobility.php">Wireless Mobility & Recovery</a></li>
-  	 				<li><a href="security.php">Security</a></li>
-  	 				<li><a href="Unified Communication.php">Unified Communication</a></li>
-  	 				<li><a href="Network Management System.php">Network Management system</a></li>
-					<li><a href="Servers.php">Server</a></li>
-					<li><a href="Storage Systems.php">Storage system</a></li>
-					<li><a href="Digital Networking.php">Digital Networking</a></li>
-  	 			</ul>
-  	 		</div>
-  	 		<div class="footer-col">
-  	 			<h4>ELV(Extra-Low-Voltagw)</h4>
-  	 			<ul>
-  	 				<li><a href="Surveillance & Video Analytics-1.php">Survillence & Video Analytics</a></li>
-  	 				<li><a href="Access Control System & Visitor Management.php">Access Control System & Recovery</a></li>
-  	 				<li><a href="Fire Detection & Suppression System -1.php">FIRE DETECTION & SUPPRESSION SYSTEM</a></li>
-  	 				<li><a href="BMS (Building Management System)-1.php">BMS(BUILDING MANAGEMENT SYSTEM)</a></li>
-					   <li><a href="#">BOOM BARRIER & PARKING MANAGEMENT</a></li>
-  	 			</ul>
-  	 		</div>
-			   <div class="footer-col">
-				<h4>Support</h4>
-				<ul>
-					<li><a href="IT Outsourcing.php">IT OUT SOURCING</a></li>
-					<li><a href="Maintenance Support.php">MAINTENANCE SUPPORT& Recovery</a></li>
-					<li><a href="#">IT CONSULTANCY</a></li>
-					<li><a href="Security Audit.php">SECURITY AUDIT</a></li>
-				</ul>
-			</div>
-			<div class="footer-col">
-				<h4>Address</h4>
-				<ul>
-					<li><a href="https://maps.app.goo.gl/Tvq7Y53jcUNrLnVu9">Australia,9 Eravatamia Street Runcorn QLD- 4113 </a></li>
-					<li><a href="https://maps.app.goo.gl/6wZDpsiAuRSfcd3W9">India 602 Pushti Heights, Shubash Chowk,Gurukul Road, Memnager,Ahemadabad,54. </a></li>
-						
-				</ul>
-			</div>
-			<div class="footer-col">
-				<h4>Contact us</h4>
-				<ul>
-					<li><a href="https://api.whatsapp.com/send/?phone=%2B61410118386&text&type=phone_number&app_absent=0">+61 410118386</a></li>
-					<li><a href="https://api.whatsapp.com/send/?phone=8000120384&text&type=phone_number&app_absent=0">M: +91 8000120384</a></li>
-					<li><a href="https://mail.google.com/mail/u/0/#inbox?compose=CllgCJZXhjKKjstrnjTwXnNtbTTcdjQWsPFrjjsmlnNLcTKDSHJrxhJPcKsvcVJFrZSRMHFxLrg">info@datapluse.co.in</a></li>
-						
-				</ul>
-			</div>
-			<div class="footer-col" style="margin-left: 571px;">
-  	 			<h4>follow us</h4>
-  	 			<div class="social-links">
-  	 				<a href="#"><i class="fab fa-facebook-f"></i></a>
-  	 				<a href="#"><i class="fab fa-twitter"></i></a>
-  	 				<a href="#"><i class="fab fa-instagram"></i></a>
-  	 				<a href="#"><i class="fab fa-linkedin-in"></i></a>
-  	 			</div>
-				   <div class="bg-light py-4">
-				  </div><div class="container text-center">
-					<p class="text-muted mb-0 py-2" style="
-  margin-left: -424px;
-  margin-top: 170px;
-  color: white;
-">© 2019 DataPluse Solution All rights reserved.</p>
-				  </div>
-				 </div>
-  	 		</div>
-  	 	</div>
-  	 </div>
+    <div class="container">
+      <div class="row">
+        
+        <div class="footer-col">
+          <h4>IT Services</h4>
+          <ul>
+            <li><a href="it-infrastucture.php">IT Infrastructure Solution</a></li>
+            <li><a href="System IntegrationServices.php">System Integration Service</a></li>
+            <li><a href="data-center.php">Data Center & Recovery</a></li>
+            <li><a href="data-availbility.php">Data Availability</a></li>
+            <li><a href="Virtualization.php">Virtualization</a></li>
+            <li><a href="CloudServices.php">Cloud Computing</a></li>
+            <li><a href="Email Spam Protections.php">Email Protection</a></li>
+          </ul>
+        </div>
+
+        <div class="footer-col">
+          <h4>Covered Network</h4>
+          <ul>
+            <li><a href="Structured Cabling System.php">Structured Cabling System</a></li>
+            <li><a href="Wireless Mobility.php">Wireless Mobility</a></li>
+            <li><a href="security.php">Security</a></li>
+            <li><a href="Unified Communication.php">Unified Communication</a></li>
+            <li><a href="Network Management System.php">Network Management System</a></li>
+            <li><a href="Servers.php">Server</a></li>
+            <li><a href="Storage Systems.php">Storage System</a></li>
+            <li><a href="Digital Networking.php">Digital Networking</a></li>
+          </ul>
+        </div>
+
+        <div class="footer-col">
+          <h4>ELV (Extra-Low-Voltage)</h4>
+          <ul>
+            <li><a href="Surveillance & Video Analytics-1.php">Surveillance & Video Analytics</a></li>
+            <li><a href="Access Control System & Visitor Management.php">Access Control System</a></li>
+            <li><a href="Fire Detection & Suppression System -1.php">Fire Detection & Suppression</a></li>
+            <li><a href="BMS (Building Management System)-1.php">Building Management System (BMS)</a></li>
+            <li><a href="#">Boom Barrier & Parking Management</a></li>
+          </ul>
+        </div>
+
+        <div class="footer-col">
+          <h4>Support</h4>
+          <ul>
+            <li><a href="IT Outsourcing.php">IT Outsourcing</a></li>
+            <li><a href="Maintenance Support.php">Maintenance Support</a></li>
+            <li><a href="#">IT Consultancy</a></li>
+            <li><a href="Security Audit.php">Security Audit</a></li>
+          </ul>
+        </div>
+
+        <div class="footer-col">
+          <h4>Address</h4>
+          <ul>
+            <li><a href="https://maps.app.goo.gl/Tvq7Y53jcUNrLnVu9">Australia: 9 Eravatamia Street, Runcorn QLD-4113</a></li>
+            <li><a href="https://maps.app.goo.gl/6wZDpsiAuRSfcd3W9">India: 602 Pushti Heights, Gurukul Road, Ahmedabad-54</a></li>
+          </ul>
+        </div>
+
+        <div class="footer-col">
+          <h4>Contact Us</h4>
+          <ul>
+            <li><a href="https://api.whatsapp.com/send/?phone=%2B61410118386">+61 410118386</a></li>
+            <li><a href="https://api.whatsapp.com/send/?phone=8000120384">+91 8000120384</a></li>
+            <li><a href="mailto:info@datapluse.co.in">info@datapluse.co.in</a></li>
+          </ul>
+        </div>
+
+        <div class="footer-col">
+          <h4>Follow Us</h4>
+          <div class="social-links">
+            <a href="#"><i class="fab fa-facebook-f"></i></a>
+            <a href="#"><i class="fab fa-twitter"></i></a>
+            <a href="#"><i class="fab fa-instagram"></i></a>
+            <a href="#"><i class="fab fa-linkedin-in"></i></a>
+          </div>
+        </div>
+
+      </div>
+
+      <!-- Copyright Section -->
+      <div class="footer-copyright">
+        <p>© 2019 DataPluse Solution. All rights reserved.</p>
+      </div>
+
+    </div>
   </footer>
 
 </body>
